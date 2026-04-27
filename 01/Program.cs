857b4142b5a6a090d83f923dd1dce26a1e3637f0
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-04-27 11:06:18 -0600

    Matriz

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("");
+﻿Console.WriteLine("Ingrese la cantidad de zonas (filas): ");
+int zonas = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la cantidad de días (columnas): ");
+int diasT = int.Parse(Console.ReadLine());
+
+double[,] temp = new double[zonas, diasT];
+
+for (int i = 0; i < zonas; i++)
+{
+    for (int j = 0; j < diasT; j++)
+    {
+        Console.Write($"Zona {i + 1}, Día {j + 1}: ");
+        temp[i, j] = double.Parse(Console.ReadLine());
+    }
+}
+
+
+
+
+
+
