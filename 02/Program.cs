6a6ea1a5edb85b8ca929e618dd049a8840d94d73
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-04-27 11:33:40 -0600

    Ejercicio 04

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Collections.Generic;
 
 Console.WriteLine("Ingrese cuantpos productos: ");
 int productos=int.Parse(Console.ReadLine());
---
+++ 03/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ingrese apartamentos: ");
+int apartamentos= int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese días: ");
+int dias = int.Parse(Console.ReadLine());
+
+double[,] consumo= new double[apartamentos,dias];
+double suma = 0;
+
+for(int i=0; i<apartamentos;i++)
+{
+    for(int j=0; j <dias; j++)
+    {
+        Console.Write($"Apartamento: {i + 1}, Consumo: {j + 1}: ");
+        consumo[i, j] = double.Parse(Console.ReadLine());
+        suma +=consumo[i,j];
+    }
+}
+Console.WriteLine(suma);
+
