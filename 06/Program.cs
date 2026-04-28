c6b475ae3d772abbca28eed496bb2eea0df2fa2c
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-04-27 22:17:47 -0600

    ejercicio 6

---
+++ 06/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Ingrese estudiantes: ");
+int estudiantes = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese dias ");
+int dias = int.Parse(Console.ReadLine());
+
+
+int[,] asist = new int[estudiantes, dias];
+
+for (int i = 0; i < estudiantes; i++)
+{
+    for (int j = 0; j < dias; j++)
+    {
+        Console.Write($"Estudiante {i}, Día {j}: ");
+        asist[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+Console.Write("Seleccione el estudiante ");
+int sel = int.Parse(Console.ReadLine());
+
+Console.Write("Registro: ");
+for (int j = 0; j <dias; j++)
+{
+    if (asist[sel, j] == 1)
+    {
+        Console.Write("Presente ");
+    }
+    else
+    {
+        Console.Write("Ausente ");
+    }
+}
+Console.WriteLine();
